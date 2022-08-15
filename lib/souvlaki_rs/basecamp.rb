43e61306e799523df3525adb87d2e65fc0d170de
Ed Porras <ep@digressed.net>
2022-08-15 22:06:57 +0200

    refactor: rework basecamp module for readability

--- lib/souvlaki_rs/basecamp.rb
+++ lib/souvlaki_rs/basecamp.rb
@@ -5,10 +5,21 @@ require 'json'
 
 module SouvlakiRS
   module Basecamp
-    USER_AGENT = 'SouvlakiRS Fetcher Notifier'
+    USER_AGENT = 'SouvlakiRS Notifier'
 
     class Comment
       def initialize(msg_head)
+        creds = SouvlakiRS::Config.get_host_info(:basecamp)
+        raise 'Unable to load basecamp credentials' if creds.nil?
+
+        @base_uri = creds[:base_uri]
+        @user_id = creds[:id]
+        @project_id = creds[:project]
+        @msg_id = creds[:msg_id]
+        @username = creds[:username]
+        @password = creds[:password]
+        @ua_email = creds[:ua_email]
+
         @msg_head = msg_head
         @text = []
       end
@@ -21,77 +32,92 @@ module SouvlakiRS
       end
 
       #
-      # post the message
+      # post a comment to the message
       def post
-        creds = SouvlakiRS::Config.get_host_info(:basecamp)
-
-        if creds.nil?
-          SouvlakiRS.logger.error('Unable to load basecamp credentials')
-          return false
-        end
-
-        # prepare fields for the message
-        msg_id = creds[:msg_id]
-        uri = URI.parse(creds[:base_uri])
+        root_uri = URI.parse(@base_uri)
 
         # set up the connection
-        Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-          headers = {
-            'User-Agent' => "#{USER_AGENT} (#{creds[:ua_email]})",
-            'Content-Type' => 'application/json'
-          }
+        Net::HTTP.start(root_uri.host, root_uri.port, use_ssl: true) do |http|
+          ids = subscriber_ids(request_message(http))
+          return post_comment(http, ids)
+        end
+      end
 
-          # get the message first - wasteful but we need to get the
-          # list of subscribers
-          uri = URI.parse("#{creds[:base_uri]}/#{creds[:id]}/api/v1/projects/#{creds[:project]}/messages/#{msg_id}.json")
-          request = Net::HTTP::Get.new(uri.request_uri, headers)
-          request.basic_auth(creds[:username], creds[:password])
-          response = http.request(request)
-          SouvlakiRS.logger.info "GET '#{uri.request_uri}' - response code #{response.code}"
+      #
+      # extract the list of subscriber ids
+      def subscriber_ids(response_body)
+        ids = JSON.parse(response_body)['subscribers'].map { |sub| sub['id'] }
+        SouvlakiRS.logger.info "Retrieved message subscriber ids: #{ids}" unless ids.empty?
+        ids
+      rescue JSON::ParserError
+        raise 'Cannot parse JSON result to determine subscriber ids'
+      end
 
-          unless response.code.eql?('200')
-            SouvlakiRS.logger.error "Unable to retrieve basecamp message #{msg_id}"
-            return false
-          end
+      #
+      # request the message
+      def request_message(http)
+        request = Net::HTTP::Get.new(request_message_uri, headers)
+        request.basic_auth(@username, @password)
+        http.request(request) do |response|
+          return response.body if response.code.eql?('200')
+        end
 
-          # get the list of subscribe ids
-          ids = []
-          begin
-            ids = JSON.parse(response.body)['subscribers'].map { |sub| sub['id'] }
-            SouvlakiRS.logger.info "Retrieved message subscriber ids: #{ids}"
-          rescue JSON::ParserError
-            SouvlakiRS.logger.error 'Cannot parse JSON result'
-            return false
-          end
+        SouvlakiRS.logger.error("Unable to retrieve basecamp message #{@msg_id} - response code #{response.code}")
+        []
+      end
 
-          # now post the message
-          uri = URI.parse("#{creds[:base_uri]}/#{creds[:id]}/api/v1/projects/#{creds[:project]}/messages/#{msg_id}/comments.json")
-          request = Net::HTTP::Post.new(uri.request_uri, headers)
-          request.basic_auth(creds[:username], creds[:password])
+      #
+      # post a new comment to the message
+      def post_comment(http, ids)
+        request = Net::HTTP::Post.new(post_comment_uri, headers)
+        request.basic_auth(@username, @password)
+        request.body = payload(make_content, ids).to_json
 
-          payload = {
-            'content' => message
-          }
-          payload['subscribers'] = ids unless ids.empty?
-          request.body = payload.to_json
-
-          # send the request
-          response = http.request(request)
-          SouvlakiRS.logger.info "POST '#{uri.request_uri}' - response code #{response.code}"
-
+        http.request(request) do |response|
           return true if response.code.eql?('201')
+
+          SouvlakiRS.logger.info("Unable to post comment - response code #{response.code}")
         end
 
         false
       end
 
       #
-      # format the message
-      def message
+      # build the payload for the comment we'll post
+      def payload(content, subscriber_ids)
+        payload = { 'content' => content }
+        payload['subscribers'] = subscriber_ids unless subscriber_ids.empty?
+        payload
+      end
+
+      #
+      # format the comment
+      def make_content
         c = "<p>#{@msg_head}<ul>"
         @text.each { |t| c << "<li>#{t}</li>" }
         c << '<ul>'
         c
+      end
+
+      #
+      # UA header
+      def headers
+        {
+          'User-Agent' => "#{USER_AGENT} (#{@ua_email})",
+          'Content-Type' => 'application/json'
+        }
+      end
+
+      #
+      # build URIs
+      def request_message_uri
+        ep = "#{@base_uri}/#{@user_id}/api/v1/projects/#{@project_id}/messages/#{@msg_id}.json"
+        URI.parse(ep).request_uri
+      end
+
+      def post_comment_uri
+        ep = "#{@base_uri}/#{@user_id}/api/v1/projects/#{@project_id}/messages/#{@msg_id}/comments.json"
+        URI.parse(ep).request_uri
       end
     end
   end
